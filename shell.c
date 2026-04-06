8b051ce27e1b7497fa780a240a61efc638d8f5e6
Darius <dariusandreieftim@gmail.com>
2026-04-06 18:01:23 +0300

    env expansion, tilde cd support and background exec

--- shell.c
+++ shell.c
@@ -39,7 +39,7 @@ int exec_with_redir(cmd_struct* command, int n_pipes, int (*pipes)[2]) {
 pid_t run_with_redir(cmd_struct* command, int n_pipes, int (*pipes)[2]) {
   pid_t child_pid = fork();
 
-  if (child_pid) {  /* We are the parent. */
+  if (child_pid) {
     switch(child_pid) {
       case -1:
         fprintf(stderr, "Oh dear.\n");
@@ -47,10 +47,59 @@ pid_t run_with_redir(cmd_struct* command, int n_pipes, int (*pipes)[2]) {
       default:
         return child_pid;
     }
-  } else {  // We are the child. */
+  } else {
     exec_with_redir(command, n_pipes, pipes);
     perror("GRESIT!");
     return 0;
+  }
+}
+
+char* expand_home_path(char* path){
+  if(path == NULL){
+    return NULL;
+  }
+
+  if(strcmp(path, "~") == 0){
+    char* home = getenv("HOME");
+    return home != NULL ? home : path;
+  }
+
+  if(strncmp(path, "~/", 2) == 0){
+    char* home = getenv("HOME");
+    if(home == NULL){
+      return path;
+    }
+
+    size_t len = strlen(home) + strlen(path) - 1 + 1;
+    char* expanded = malloc(len);
+    if(expanded == NULL){
+      return path;
+    }
+
+    snprintf(expanded, len, "%s/%s", home, path+2);
+    return expanded; 
+  }
+  return path;
+}
+
+char* expand_env_arg(char* arg){
+  if(arg == NULL){
+    return NULL;
+  }
+  if(arg[0] == '$' && strlen(arg)>1){
+    char* value = getenv(arg+1);
+    if(value != NULL){
+      return value;
+    }
+    return "";
+  }
+
+  return arg;
+}
+
+void expand_env_vars_in_command(cmd_struct* command){
+  for(int i=0; command->args[i] != NULL; i++){
+    command->args[i] = expand_env_arg(command->args[i]);
   }
 }
 
@@ -72,18 +121,34 @@ int main() {
     if(prompt_and_get_input(prompt, &line, &len) <= 0){
       break;
     }
-    if(strcmp(line, "\n") != 0 && strcmp(line, "") != 0){
+
+    int background = 0;
+
+    line[strcspn(line, "\n")] = '\0';
+
+    size_t line_len = strlen(line);
+    if(line_len > 0 && line[line_len - 1] == '&'){
+      background = 1;
+      line[line_len - 1] = '\0';
+
+      while(line_len > 1 && line[line_len - 2] == ' '){
+        line[line_len - 2] = '\0';
+        line_len --;
+      }
+    }
+
+    if(strcmp(line, "") != 0){
       if(history_count < MAX_HISTORY){
         history[history_count] = strdup(line);
         history_count++;
       }
     }
 
-    if(strcmp(line, "\n") == 0 || strcmp(line, "") == 0){
+    if(strcmp(line, "") == 0){
       continue;
     }
 
-    if(strcmp(line, "help\n") == 0 || strcmp(line, "help") == 0){
+    if(strcmp(line, "help") == 0){
       printf("Built-in commands:\n");
       printf(" exit - exit shell\n");
       printf(" cd [dir] - change directory\n");
@@ -92,46 +157,51 @@ int main() {
       continue;
     }
 
-    if(strcmp(line, "exit\n")==0 || strcmp(line, "exit")==0){
+    if(strcmp(line, "exit")==0){
     break;
     }
 
-    if(strcmp(line, "pwd\n") == 0 || strcmp(line, "pwd") == 0){
+    if(strcmp(line, "pwd") == 0){
       if(getcwd(cwd, sizeof(cwd)) != NULL){
         printf("%s\n", cwd);
       }else{
-        perror("GRESIT");
+        perror("pwd");
       }
       continue;
     }
 
 
-    if(strcmp(line, "cd\n") == 0 || strcmp(line, "cd") == 0){
+    if(strcmp(line, "cd") == 0){
       char *home = getenv("HOME");
       if(home == NULL){
-      fprintf(stderr, "GRESIT: HOME nu exista!\n");
+      fprintf(stderr, "cd : HOME not set\n");
       }else if(chdir(home)!=0){
-       perror("GRESIT");
+       perror("cd");
       }
       continue;
     }
 
-    if(strcmp(line, "cd ~\n") == 0 || strcmp(line, "cd ~") == 0){
+    if(strcmp(line, "cd ~") == 0){
       char *home = getenv("HOME");
       if(home == NULL){
-        fprintf(stderr, "GRESIT: HOME nu exista!\n");
+        fprintf(stderr, "cd: HOME not set\n");
       }else if(chdir(home) != 0){
-        perror("GRESIT");
+        perror("cd");
       }
       continue;
     }
 
     if(strncmp(line, "cd ", 3) == 0){
-      line[strcspn(line, "\n")] = '\0';
       char *path = line+3;
+      path = expand_env_arg(path);
 
-      if(chdir(path) != 0){
-       perror("GRESIT");
+      char* expanded_path = expand_home_path(path);
+      if(chdir(expanded_path) != 0){
+       perror("cd");
+      }
+
+      if(expanded_path != path){
+        free(expanded_path);
       }
       continue;
     }
@@ -147,10 +217,15 @@ int main() {
     }
 
     pipeline_struct* pipeline = parse_pipeline(line);
-    int n_pipes = pipeline->n_cmds - 1;
-    //print_pipeline(pipeline);
 
-    /* pipes[i] redirects from pipeline->cmds[i] to pipeline->cmds[i+1]. */
+    for(int i=0;i<pipeline->n_cmds;++i){
+      expand_env_vars_in_command(pipeline->cmds[i]);
+    }
+
+    int n_pipes = pipeline->n_cmds - 1;
+
+
+
     int (*pipes)[2] = calloc(sizeof(int[2]), n_pipes);
 
     for (int i = 1; i < pipeline->n_cmds; ++i) {
@@ -165,11 +240,12 @@ int main() {
 
     close_ALL_the_pipes(n_pipes, pipes);
 
-    /* Wait for all the children to terminate. Rule 0: not checking status. */
-    for (int i = 0; i < pipeline->n_cmds; ++i) {
-      wait(NULL);
+    if(!background){
+        for (int i = 0; i < pipeline->n_cmds; ++i) {
+          wait(NULL);
+        }
+      }
     }
-  }
   fputs("\n", stderr);
   return 0;
 }
